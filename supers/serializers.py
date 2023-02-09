7f455a02bdb62485da329d4fcc720a55f49f784c
Whomerding <wendy.homerding@gmail.com>
2023-02-09 05:11:54 -0500

    updated urls in supers app and in the project folder

--- supers/serializers.py
+++ supers/serializers.py
@@ -3,7 +3,7 @@
 from rest_framework import serializers
 from .models import Super
 
-class CarSerializer(serializers.ModelSerializer):
+class SupersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
         fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
--- supers/urls.py
+++ supers/urls.py
@@ -1,1 +1,9 @@
 
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.supers_list),
+    path('<int:pk>/', views.super_detail)
+]
