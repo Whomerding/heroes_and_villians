50def6c6ba564f19b7bcb04686d93c884b3bc71b
Whomerding <wendy.homerding@gmail.com>
2023-02-10 12:36:46 -0500

    fixed the serialier for supers to post new superheros

--- supers/serializers.py
+++ supers/serializers.py
@@ -6,4 +6,8 @@ from .models import Super
 class SupersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
+        depth = 1
+    super_type_id = serializers.IntegerField(write_only=True)
+    
+        
